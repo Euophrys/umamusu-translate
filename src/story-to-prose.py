5591d4f08721c4c0d757985bf49b25a8adfde297
Amber Flina <erzz197+git@gmail.com>
2022-12-02 09:08:26 -0400

    Support branching

--- src/story-to-prose.py
+++ src/story-to-prose.py
@@ -19,12 +19,20 @@ def convert(file):
         output_directory = '\\'.join(file.replace('translations', args.dst).split('\\')[0:-1])
         output_destination = file.replace('translations', args.dst).replace('json', 'txt')
         Path(output_directory).mkdir(parents=True, exist_ok=True)
+        branches = 0
         with open(output_destination, 'w', encoding='utf-8') as output:
             data = json.load(jsonFile)
             # title of this chapter
             output.write('～ %s ～\n\n' % data['title'])
             last_speaker = 'none'
+            branching_choices = []
+            choices_shown = 1
+            skip_to = -1
+
             for message in data['text']:
+                if message['blockIdx'] < skip_to:
+                    continue
+
                 # just output female lines
                 if 'trainerGender' in message and message['trainerGender'] != args.gender:
                     continue
@@ -49,16 +57,44 @@ def convert(file):
                 
                 # if the trainer has something to say add them in
                 if 'choices' in message:
+                    choices = condenseChoices(message['choices'])
                     if not is_monologue(last_speaker):
                         output.write('」')
-                    # In uma stories, the first choice is for male trainers and second is for female
-                    if args.gender == 'm':
-                        output.write('\n\nトレーナー: 「%s' % message['choices'][0]['jpText'])
-                    else:
-                        output.write('\n\nトレーナー: 「%s' % message['choices'][-1]['jpText'])
+                    output.write('\n\n')
+
+                    if not all(choice['nextBlock'] == choices[0]['nextBlock'] for choice in choices):
+                        branching_choices = list(choices)
+                        skip_to = branching_choices[0]['nextBlock']
+                        branching_choices.pop(0)
+                        output.write('# CHOICE 1 #\n')
+
+                    output.write('トレーナー: 「%s' % choices[0]['jpText'])
                     last_speaker = 'トレーナー'
-            if not is_monologue(last_speaker):
-                output.write('」')
+                
+                if message['nextBlock'] == -1:
+                    if not is_monologue(last_speaker):
+                        output.write('」')
+                    
+                    if len(branching_choices) > 0:
+                        choices_shown += 1
+                        skip_to = branching_choices[0]['nextBlock']
+                        output.write('\n\n # CHOICE %d #\nトレーナー: 「%s' % (choices_shown, branching_choices.pop(0)['jpText']))
+                        last_speaker = 'トレーナー'
+
+def condenseChoices(choices:list):
+    present_numbers = set()
+    for choice in choices:
+        present_numbers.add(choice['nextBlock'])
+    condensed_choices = []
+
+    # If two choices go to the same place, the latter choice is for female trainers
+    if args.gender == "f":
+        choices.reverse()
+
+    for number in sorted(present_numbers):
+        condensed_choices.append(next(i for i in choices if i['nextBlock'] == number))
+
+    return condensed_choices
 
 def main():
     parseArgs()
